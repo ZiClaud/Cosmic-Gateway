28d30417ffa7b2fb3797b23a221efc97eac1ce4b
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2024-02-14 16:32:48 +0100

    Game is now almost finished

--- EnemySpawner.gd
+++ EnemySpawner.gd
@@ -1,6 +1,7 @@
 extends Node2D
 # Spawn every enemy every second
 
+@export var meteor_tscn: PackedScene
 @export var enemy_tscn: PackedScene
 @export var start_time: float = 3.0
 @export var min_spawn_time: float = 0.5
@@ -13,6 +14,16 @@ var curr_times: int = 0
 
 func _ready():
 	timer.wait_time = start_time
+
+func spawn_meteor():
+	var new_meteor = meteor_tscn.instantiate()
+	
+	var viewport_height:int = get_viewport_rect().size.y
+	var rand_y:int = randi_range(0, viewport_height)
+	new_meteor.position.x = -100
+	new_meteor.position.y = rand_y
+	
+	self.add_child(new_meteor)
 
 
 func spawn_enemy():
@@ -27,7 +38,14 @@ func spawn_enemy():
 
 
 func _on_timer_timeout():
-	spawn_enemy()
+	if (CurrGame.getLevel() <= 2):
+		spawn_meteor()
+	elif (CurrGame.getLevel() == 3):
+		spawn_enemy()
+	else:
+		spawn_meteor()
+		spawn_enemy()
+	
 	curr_times += 1
 	if curr_times > speedup_after:
 		curr_times = 0
