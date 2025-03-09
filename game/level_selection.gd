e8baf5aea042832868f8c50a9e54f31add6e418d
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2025-03-09 18:43:45 +0100

    You can now unlock all levels in settings

--- game/level_selection.gd
+++ game/level_selection.gd
@@ -2,12 +2,15 @@ extends Node
 
 var _unlocked_levels:int = 1
 
+
 func reset_values():
 	_unlocked_levels = 0
+
 
 func unlockLevel(level:int):
 	if level >= _unlocked_levels:
 		_unlocked_levels = level
+
 
 func getUnlockedLevels():
 	return _unlocked_levels
--- ui/settings_page.gd
+++ ui/settings_page.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var movement_bt: Button = %MovementButton
+@onready var unlock_container: HBoxContainer = %LevelsUnlockContainer
 
 
 func update_movement_bt_text():
@@ -11,6 +12,8 @@ func update_movement_bt_text():
 
 
 func _ready():
+	if LevelSelection.getUnlockedLevels() >= 4:
+		unlock_container.visible = false
 	update_movement_bt_text()
 
 
@@ -21,3 +24,8 @@ func _on_back_button_pressed():
 func _on_movement_button_pressed():
 	Utils.toggle_movement_default()
 	update_movement_bt_text()
+
+
+func _on_unlock_button_pressed():
+	LevelSelection.unlockLevel(4)
+	unlock_container.visible = false
