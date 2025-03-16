e2ceeaab8096d1023a441f7d26fd731cfb1bd60f
Claudio Di Maio <56027915+ZiClaud@users.noreply.github.com>
2025-03-16 11:42:40 +0100

    Fixing lag problems - Entities

--- game/EnemySpawner.gd
+++ game/EnemySpawner.gd
@@ -1,10 +1,11 @@
 extends Node2D
 # Spawn every enemy every second
 
-@export var meteor_tscn: PackedScene
-@export var ind_meteor_tscn: PackedScene
-@export var enemy_tscn: PackedScene
-@export var boss_tscn: PackedScene
+const METEOR_SCENE = preload("res://enemies/meteors/meteor_big.tscn")
+const INDESTRUCTIBLE_METEOR_SCENE = preload("res://enemies/meteors/meteor_indescructible.tscn")
+const ENEMY_SCENE = preload("res://enemies/enemy.tscn")
+const BOSS_SCENE = preload("res://enemies/boss.tscn")
+
 @export var start_time: float = 3.0
 @export var min_spawn_time: float = 0.5
 @export var speedup_after: int = 5
@@ -16,31 +17,41 @@ var curr_times: int = 0
 var stage1: bool = false
 var stage2: bool = false
 
+
 func _ready():
 	timer.wait_time = start_time
 	indistrucible_spawn()
 
 
+func _spawn(scene: PackedScene):
+	var new_entity = scene.instantiate()
+	var viewport_height = get_viewport_rect().size.y
+	new_entity.position = Vector2(-100, randi_range(0, viewport_height))
+	call_deferred("add_child", new_entity)
+
+
 func spawn_meteor():
-	var new_meteor = meteor_tscn.instantiate()
-	
-	var viewport_height:int = get_viewport_rect().size.y
-	var rand_y:int = randi_range(0, viewport_height)
-	new_meteor.position.x = -100
-	new_meteor.position.y = rand_y
-	
-	self.add_child(new_meteor)
+	_spawn(METEOR_SCENE)
 
 
 func spawn_enemy():
-	var new_enemy = enemy_tscn.instantiate()
-	
-	var viewport_height:int = get_viewport_rect().size.y
-	var rand_y:int = randi_range(0, viewport_height)
-	new_enemy.position.x = -100
-	new_enemy.position.y = rand_y
-	
-	self.add_child(new_enemy)
+	_spawn(ENEMY_SCENE)
+
+
+func _spawn_boss():
+	_spawn(BOSS_SCENE)
+
+
+func spawn_boss():
+	if (CurrGame.getScore() > 1000 && !stage1):
+		print("Stage 1!")
+		_spawn_boss()
+		stage1=true
+	elif (CurrGame.getScore() > 3000 && !stage2):
+		_spawn_boss()
+		_spawn_boss()
+		_spawn_boss()
+		stage2=true
 
 
 func _on_timer_timeout():
@@ -61,43 +72,7 @@ func _on_timer_timeout():
 
 
 func indistrucible_spawn():
-	var new_meteor1 = ind_meteor_tscn.instantiate()
-	var new_meteor2 = ind_meteor_tscn.instantiate()
-	
-	var viewport_height:int = get_viewport_rect().size.y
-	new_meteor1.position.x = -100
-	new_meteor1.position.y = randi_range(0, viewport_height)
-	new_meteor2.position.x = -100
-	new_meteor2.position.y = randi_range(0, viewport_height)
-	
-	if (CurrGame.getLevel() == 2):
-		self.add_child(new_meteor1)
-	elif (CurrGame.getLevel() == 3):
-		self.add_child(new_meteor1)
-		self.add_child(new_meteor2)
-	elif (CurrGame.getLevel() == 4):
-		self.add_child(new_meteor1)
-		self.add_child(new_meteor2)
-
-
-func spawn_boss():
-	if (CurrGame.getScore() > 1000 && !stage1):
-		print("Stage 1!")
-		_spawn_boss()
-		stage1=true
-	elif (CurrGame.getScore() > 3000 && !stage2):
-		_spawn_boss()
-		_spawn_boss()
-		_spawn_boss()
-		stage2=true
-
-
-func _spawn_boss():
-	var new_boss = boss_tscn.instantiate()
-	
-	var viewport_height:int = get_viewport_rect().size.y
-	var rand_y:int = randi_range(0, viewport_height)
-	new_boss.position.x = -100
-	new_boss.position.y = rand_y
-	
-	self.add_child(new_boss)
+	if CurrGame.getLevel() >= 2:
+		_spawn(INDESTRUCTIBLE_METEOR_SCENE)
+	if CurrGame.getLevel() >= 3:
+		_spawn(INDESTRUCTIBLE_METEOR_SCENE)
